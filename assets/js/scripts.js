3bc0dea017a610d3994b3b173c453c6e1643454b
Datum <allen.james.west@gmail.com>
2020-04-10 17:23:31 +0000

    working on maps API still not integrating correctly

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 //<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDV2rXpc-RvQizNTihXs0xXtVik0nRkmE8&callback=initMap"></script>
 //Maps API 
+var map;
    function initMap(){
  map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 40.975, lng: -75.139},
