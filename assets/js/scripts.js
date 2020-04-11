72eef1fea8bd55e9e816c20c1a87fd6b02f77960
Datum <allen.james.west@gmail.com>
2020-04-11 20:35:19 +0000

    got maps working by setting min-height, working on night time to day time transition.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
     $('#greeting').modal('show');
     });
-$('#greeting').click(function(){
-    //$('#greeting').addClass(fade);
-    //$('#greeting').modal('hide');
+$('#greeting').fadeOut(2000,function() {
+  $(this).modal('hide');
 });
+//$( "#greeting" ).click(function() {
+  //$( "#greeting" ).fadeOut( "slow" );
+//});
+$(document).ready(function(){
+    $('#day').on ("click", function(){
+    $('body').removeClass("DayTime");
+    $('#dark').removeClass("btn-dark");
+    $('body').addClass("afterDark");
+    $('#dark').addClass("btn-light");
+    $('#dark').text("After Dark");
+    $("body").attr('id', 'dark');
+    $("button").attr('id', 'night');  
+    });
+});
+ $('#night').on ("click", function(){
+    $('body').removeClass("afterDark");
+    $('#dark').removeClass("btn-light");
+    $('body').addClass("DayTime");
+    $('#dark').addClass("btn-dark");
+    $('#dark').text("Day Time");
+    $("body").attr('id', 'light');
+    $("button").attr('id', 'day');  
+    });
