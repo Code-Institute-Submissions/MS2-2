ddefe66f040857d3da2f2ee874771d9515f9afd2
Datum <allen.james.west@gmail.com>
2020-04-12 22:49:14 +0000

    starting content change during night mode activation and added fixed background, attempting to start using REI's API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,11 +15,26 @@ $(document).ready(function(){
           $('#lightswitch').removeClass("btn-dark").addClass("btn-light");
           $('#lightswitch').text("After Dark");
           $("body").attr('id', 'dark');
+          $("div.information").fadeOut("slow");
+          //$("div").
           }
         else{
           $('body').removeClass("afterDark",).addClass("DayTime");
           $('#lightswitch').removeClass("btn-light").addClass("btn-dark");
           $('#lightswitch').text("Day Time");
           $("body").attr('id', 'light');
+           $("div.information").fadeIn("slow");
     }
     })});
+
+
+    // maps code below
+	var APIkey = "AIzaSyDV2rXpc-RvQizNTihXs0xXtVik0nRkmE8";
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: { lat: 40.975, lng: -75.139 }
+        
+        });
+    }
