84077d9d17282343a0593f580f5d9c0f45b14cb4
Datum <allen.james.west@gmail.com>
2020-04-12 20:00:28 +0000

    got night mode functioning, now to add night mode content.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,22 +8,18 @@ $('#greeting').fadeOut(2000,function() {
   //$( "#greeting" ).fadeOut( "slow" );
 //});
 $(document).ready(function(){
-    $('#day').on ("click", function(){
-    $('body').removeClass("DayTime");
-    $('#dark').removeClass("btn-dark");
-    $('body').addClass("afterDark");
-    $('#dark').addClass("btn-light");
-    $('#dark').text("After Dark");
-    $("body").attr('id', 'dark');
-    $("button").attr('id', 'night');  
-    });
-});
- $('#night').on ("click", function(){
-    $('body').removeClass("afterDark");
-    $('#dark').removeClass("btn-light");
-    $('body').addClass("DayTime");
-    $('#dark').addClass("btn-dark");
-    $('#dark').text("Day Time");
-    $("body").attr('id', 'light');
-    $("button").attr('id', 'day');  
-    });
+    $('#lightswitch').on ("click", function(){
+          if 
+          ($("body").hasClass("DayTime")){
+          $('body').removeClass("DayTime").addClass("afterDark");
+          $('#lightswitch').removeClass("btn-dark").addClass("btn-light");
+          $('#lightswitch').text("After Dark");
+          $("body").attr('id', 'dark');
+          }
+        else{
+          $('body').removeClass("afterDark",).addClass("DayTime");
+          $('#lightswitch').removeClass("btn-light").addClass("btn-dark");
+          $('#lightswitch').text("Day Time");
+          $("body").attr('id', 'light');
+    }
+    })});
