98733043e6b04884571b8efbd6fb26d827ae1784
Datum <allen.james.west@gmail.com>
2020-05-15 19:58:53 +0000

    working on data capture with help from Samantha@CI

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -70,33 +70,35 @@ function markerMaker(map) {
             map: map,
             title: titleTwo,
         });
-        let markerInfo = '<div class="customMarker"><h4>' + myData[i].name + '<button id="' + [i] + '" ><i class="fas fa-plus-circle add"></i></button>' + '</h4><h6> distance to Springer Mountain: ' + myData[i]["to spgr"] + '</h6><h6> distance to Mt. Katahdin: ' + myData[i]["to ktd"] + '</h6></div>';
+        let markerInfo = '<div class="customMarker"><h4>' + myData[i].name + '<button id="add" ><i class="fas fa-plus-circle add"></i></button>' + '</h4><h6> distance to Springer Mountain: ' + myData[i]["to spgr"] + '</h6><h6> distance to Mt. Katahdin: ' + myData[i]["to ktd"] + '</h6></div>';
         let infowindow = new google.maps.InfoWindow({
             content: markerInfo
         });
 
         marker.addListener('click', function () {
             infowindow.open(marker.get('map'), this);
-        });
+
+        $('.add').on('click', function (e) {
+            let markerContent = document.getElementById('#add');
 
 
+            let li = "<li>" + markerContent + "</li>";
+            //console.log(li);
+            $('#list').append(li);
 
-
-    };
-    $('.add').on('click', function () {
-        let ol = document.getElementById("list").innerHTML;
-        let li = "<li>" + markerInfo + "</li>";
-        console.log(li);
-        $('#list').append(li);
-
+            });
+        let e = document.getElementById("#add") ;
     });
+
+
+
+
+};
+
 
 };
 
 // start list functions
-
-
-
 
 
 
